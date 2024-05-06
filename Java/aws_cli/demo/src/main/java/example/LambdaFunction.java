449f44aef7b43e446dc59c0027cf0512daf13a4a
Saket Chaudhary <saketchaudharynit@gmail.com>
2024-05-06 12:23:23 +0530

    update

---
+++ Java/aws_cli/demo/src/main/java/example/LambdaFunction.java
@@ -0,0 +1,18 @@
+package example;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.Map;
+
+public class LambdaFunction implements RequestHandler<Map<String,String>, Void>{
+
+  @Override
+  public Void handleRequest(Map<String,String> event, Context context)
+  {
+    LambdaLogger logger = context.getLogger();
+    logger.log("EVENT TYPE: " + event.getClass());
+    return null;
+  }
+}
